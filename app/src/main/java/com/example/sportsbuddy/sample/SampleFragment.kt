2945ed919130f65253bc28f81f9ba82eab1b6a13
blueme0 <ksytpdus0304@gmail.com>
2024-06-05 20:02:16 +0900

    [feat] sample fragment for test navigation bar

---
+++ app/src/main/java/com/example/sportsbuddy/sample/SampleFragment.kt
@@ -0,0 +1,26 @@
+package com.example.sportsbuddy.sample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sportsbuddy.R
+import com.example.sportsbuddy.databinding.FragmentMatchListBinding
+import com.example.sportsbuddy.databinding.FragmentSampleBinding
+import com.example.sportsbuddy.matchlist.MatchListAdapter
+
+class SampleFragment : Fragment() {
+
+    private var _binding : FragmentSampleBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentSampleBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
